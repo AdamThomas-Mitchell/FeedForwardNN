600e519a56e4be658f709195598d3e4d88f06c67
AdamThomas-Mitchell <athomasmitchell017@gmail.com>
2024-11-11 20:26:56 +0000

    Updated functions for downloading MNIST raw data

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -2,40 +2,50 @@ import logging
 from urllib.request import urlretrieve
 from urllib.error import URLError, HTTPError, ContentTooShortError
 
+
+def download_from_url(url: str, file_path: str, max_attempts: int = 3) -> None:
+    """Download file from url and save to specified location
+
+    Args:
+        url (str): URL of file to be downloaded
+        file_path (str): path to where the downloaded file is saved
+        max_attempts (int, optional): maximum number of times to retry the URL download if issues occur. Defaults to 3.
+    """
+    logging.info(f'Downloading file from {url} and saving to {file_path}...')
+    for retry_attempt in range(max_attempts):
+        try:
+            urlretrieve(url, file_path)
+            logging.info('File successfully downloaded')
+            break
+        except HTTPError as ex:
+            if retry_attempt < (max_attempts - 1):
+                logging.warning(f'HTTP error when attempting to download file from {url}: {ex.code} - {ex.reason}')
+                logging.info('Retrying...')
+            else:
+                logging.error(f'Failed to download file from {url}: {ex.code} - {ex.reason}')
+        except URLError as ex:
+            if retry_attempt < (max_attempts - 1):
+                logging.warning(f'Failed to download file from {url}: {ex.reason}')
+                logging.info('Retrying...')
+            else:
+                logging.error(f'Failed to download file from {url}: {ex.reason}')
+        except ContentTooShortError:
+            if retry_attempt < (max_attempts - 1):
+                logging.warning(f'Amount of downloaded data from {url} is less than the expected amount')
+                logging.info('Retrying...')
+            else:
+                logging.warning(f'Amount of downloaded data from {url} is less than the expected amount - data may not be correct')
+
 def download_mnist():
-    """Method to download MNIST raw data
+    """Download the 4 raw data files for the MNIST dataset - train images, train labels, test images, test labels
     """
     TRAIN_IMAGES_ADDRESS = 'https://storage.googleapis.com/cvdf-datasets/mnist/train-images-idx3-ubyte.gz'
     TRAIN_LABELS_ADDRESS = 'https://storage.googleapis.com/cvdf-datasets/mnist/train-labels-idx1-ubyte.gz'
     TEST_IMAGES_ADDRESS = 'https://storage.googleapis.com/cvdf-datasets/mnist/t10k-images-idx3-ubyte.gz'
     TEST_LABELS_ADDRESS = 'https://storage.googleapis.com/cvdf-datasets/mnist/t10k-labels-idx1-ubyte.gz'
-    MAX_ATTEMPTS = 3
     
     logging.info('Downloading MNIST raw data...')
-    for retry_attempt in range(MAX_ATTEMPTS):
-        try:
-            # TODO: should I have a separate function for general urlretrive from given url?
-            urlretrieve(TRAIN_IMAGES_ADDRESS, 'src/datasets/mnist/raw/train-images-idx3-ubyte.gz')
-            urlretrieve(TRAIN_LABELS_ADDRESS, 'src/datasets/mnist/raw/train-labels-idx3-ubyte.gz')
-            urlretrieve(TEST_IMAGES_ADDRESS, 'src/datasets/mnist/raw/test-images-idx3-ubyte.gz')
-            urlretrieve(TEST_LABELS_ADDRESS, 'src/datasets/mnist/raw/test-labels-idx3-ubyte.gz')
-            logging.info('MNIST raw data successfully downloaded to src/datasets/mnist/raw')
-            break
-        except HTTPError as ex:
-            if retry_attempt < (MAX_ATTEMPTS - 1):
-                logging.warning(f'HTTP error when attempting to download MNIST data: {ex.code} - {ex.reason}')
-                logging.info('Retrying...')
-            else:
-                logging.error(f'Failed to download MNIST data - {ex.code} - {ex.reason}')
-        except URLError as ex:
-            if retry_attempt < (MAX_ATTEMPTS - 1):
-                logging.warning(f'Failed to download MNIST data: {ex.reason}')
-                logging.info('Retrying...')
-            else:
-                logging.error(f'Failed to download MNIST data: {ex.reason}')
-        except ContentTooShortError:
-            if retry_attempt < (MAX_ATTEMPTS - 1):
-                logging.warning('Amount of downloaded data is less than the expected amount')
-                logging.info('Retrying...')
-            else:
-                logging.warning('Amount of downloaded data is less than the expected amount - data may not be correct')        
+    download_from_url(TRAIN_IMAGES_ADDRESS, 'src/datasets/mnist/raw/train-images-idx3-ubyte.gz')
+    download_from_url(TRAIN_LABELS_ADDRESS, 'src/datasets/mnist/raw/train-labels-idx3-ubyte.gz')
+    download_from_url(TEST_IMAGES_ADDRESS, 'src/datasets/mnist/raw/test-images-idx3-ubyte.gz')
+    download_from_url(TEST_LABELS_ADDRESS, 'src/datasets/mnist/raw/test-labels-idx3-ubyte.gz')       
